3f70f448dbb65d0b1e07f0bf69b230a3abcb4b1b
Bilal Aydın <bilalaydibn414@gmail.com>
2025-01-25 14:15:01 +0300

    user location retrieved

--- TreavlBook/AddVC.swift
+++ TreavlBook/AddVC.swift
@@ -7,8 +7,9 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
-class AddVC: UIViewController, MKMapViewDelegate {
+class AddVC: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
     
     @IBOutlet weak var placeName: UITextField!
     @IBOutlet weak var placeCommend: UITextField!
@@ -18,11 +19,24 @@ class AddVC: UIViewController, MKMapViewDelegate {
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var mapView: MKMapView!
     
+    var localManager = CLLocationManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         mapView.delegate = self
+        localManager.delegate = self
+        localManager.requestWhenInUseAuthorization()
+        localManager.desiredAccuracy = kCLLocationAccuracyBest
+        localManager.startUpdatingLocation()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        let location = CLLocationCoordinate2D(latitude: locations[0].coordinate.latitude, longitude: locations[0].coordinate.latitude)
+        let span = MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
+        let region = MKCoordinateRegion(center: location, span: span)
+        mapView.setRegion(region, animated: true)
     }
     
 
