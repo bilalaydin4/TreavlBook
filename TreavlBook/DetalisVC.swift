df5bf30ee808d75036f5b7144755b4c78cbf7676
Bilal Aydın <bilalaydibn414@gmail.com>
2025-01-26 03:24:51 +0300

    Detail page data fetched, delete operation added.

--- TreavlBook/DetalisVC.swift
+++ TreavlBook/DetalisVC.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import MapKit
+import CoreData
 
 class DetalisVC: UIViewController, MKMapViewDelegate {
     
@@ -15,13 +16,131 @@ class DetalisVC: UIViewController, MKMapViewDelegate {
     @IBOutlet weak var mapView: MKMapView!
     
 
+    var chosenPlace = ""
+    var chosenId : UUID?
+    
+    var annotationTitle : String?
+    var annotationSubtitle : String?
+    var annotationLatitude : Double?
+    var annotationLongitude : Double?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         mapView.delegate = self
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .trash, target: self, action: #selector(deletePlace))
+        
+        
+        
+        if chosenId != nil && chosenPlace != "" {
+            
+            let appDelegate = UIApplication.shared.delegate as! AppDelegate
+            let context = appDelegate.persistentContainer.viewContext
+            let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Places")
+            
+            let idString = chosenId?.uuidString
+            fetchRequest.predicate = NSPredicate(format: "id == %@", idString!)
+            fetchRequest.returnsObjectsAsFaults = false
+            
+            do {
+                let results = try context.fetch(fetchRequest)
+                
+                if results.count > 0 {
+                    for result in results as! [NSManagedObject] {
+                        if let title = result.value(forKey: "name") as? String {
+                            annotationTitle = title
+                            
+                            if let subTitle = result.value(forKey: "comment") as? String {
+                                annotationSubtitle = subTitle
+                                
+                                if let latitude = result.value(forKey: "latitude") as? Double {
+                                    annotationLatitude = latitude
+                                    
+                                    if let longitude = result.value(forKey: "longitude") as? Double {
+                                        annotationLongitude = longitude
+                                        
+                                        if let imageData = result.value(forKey: "image") as? Data {
+                                            imageView.image = UIImage(data: imageData)
+                                            self.navigationItem.title = annotationTitle
+                                            
+                                            let annotation = MKPointAnnotation()
+                                            let coordinate = CLLocationCoordinate2D(latitude: annotationLatitude!, longitude: annotationLongitude!)
+                                            annotation.title = annotationTitle
+                                            annotation.subtitle = annotationSubtitle
+                                            annotation.coordinate = coordinate
+                                            mapView.addAnnotation(annotation)
+                                            
+                                            infoTextView.text = "\(annotationTitle!) , \n\(annotationSubtitle!) \n ---------------- \n latitude:  \(annotationLatitude!) \n longitude: \(annotationLongitude!) "
+                                            infoTextView.isEditable = false
+                                            let span = MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
+                                            let region = MKCoordinateRegion(center: coordinate, span: span)
+                                            mapView.setRegion(region, animated: true)
+                                            
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                
+            }catch {
+                print("Error")
+            }
+        }
     }
     
 
- 
+    @objc func deletePlace () {
+        
+        let alert = UIAlertController(title: "Deletion Success", message: "Place deleted", preferredStyle: UIAlertController.Style.alert )
+        let cancelButton = UIAlertAction(title: "Cancel", style: UIAlertAction.Style.cancel)
+        let okbutton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default) { UIAlertAction in
+            
+            let appDelegate = UIApplication.shared.delegate as! AppDelegate
+            let context = appDelegate.persistentContainer.viewContext
+            let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Places")
+            
+            let idString = self.chosenId!.uuidString
+            fetchRequest.predicate = NSPredicate(format: "id == %@", idString)
+            fetchRequest.returnsObjectsAsFaults = false
+            
+            do {
+                let results = try context.fetch(fetchRequest)
+                
+                if results.count > 0 {
+                    
+                    for result in results as! [NSManagedObject] {
+                        if let id = result.value(forKey: "id") as? UUID {
+                            if id == self.chosenId {
+                                context.delete(result)
+                                
+                                do {
+                                    try context.save()
+                                    print("Delete Success :)")
+                                    
+                                    NotificationCenter.default.post(name: NSNotification.Name("deleateData"), object: nil)
+                                    self.navigationController?.popViewController(animated: true)
+                                   
+                                }catch{
+                                    print("Error")
+                                }
+                            }
+                        }
+                        
+                    }
+                    
+                }
+            }catch {
+                print("Error")
+            }
+            
+        }
+        alert.addAction(okbutton)
+        alert.addAction(cancelButton)
+        self.present(alert, animated: true, completion: nil)
+        
+       
+    }
 
 }
--- TreavlBook/ViewController.swift
+++ TreavlBook/ViewController.swift
@@ -15,6 +15,9 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     var NameArray : [String] = []
     var IdArray : [UUID] = []
     
+    var selectedName = ""
+    var selectedId : UUID?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -42,7 +45,60 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         cell.textLabel?.text = NameArray[indexPath.row]
         return cell
     }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        selectedId = IdArray[indexPath.row]
+        selectedName = NameArray[indexPath.row]
+        performSegue(withIdentifier: "toDetalisVC", sender: nil)
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "toDetalisVC" {
+            let destinationVC = segue.destination as! DetalisVC
+            destinationVC.chosenId = selectedId
+            destinationVC.chosenPlace = selectedName
+        }
+    }
     
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            
+            let appDelegate = UIApplication.shared.delegate as! AppDelegate
+            let context = appDelegate.persistentContainer.viewContext
+            let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Places")
+            
+            let idString = IdArray[indexPath.row].uuidString
+            fetchRequest.predicate = NSPredicate(format: "id == %@", idString)
+            fetchRequest.returnsObjectsAsFaults = false
+            
+            do {
+                let results = try context.fetch(fetchRequest)
+                
+                if results.count > 0 {
+                    for result in results as! [NSManagedObject] {
+                        if let id = result.value(forKey: "id") as? UUID {
+                            if id == IdArray[indexPath.row] {
+                                
+                                IdArray.remove(at: indexPath.row)
+                                NameArray.remove(at: indexPath.row)
+                                context.delete(result)
+                                self.tableView.reloadData()
+                                
+                                do {
+                                    try context.save()
+                                    print("Delete Success :)")
+                                }catch {
+                                    print("Erorrs")
+                                }
+                            }
+                        }
+                    }
+                }
+            }catch {
+                print("Erorr")
+            }
+            
+        }
+    }
     
     
 
